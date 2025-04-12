0345c912c6cc9fa1df2661aca6aa6a7d84adced1
tippity <91023591+tippityfiddler@users.noreply.github.com>
2025-04-12 16:53:10 +0100

    Update whitelisted_users.lua

--- anomaly/whitelisted_users.lua
+++ anomaly/whitelisted_users.lua
@@ -1,6 +1,6 @@
 return {
   --> This a shit whitelist I know AND REALLY bypassble...
   "6589EC8F-DD55-4A28-A098-9E1766F8FC44", 
-  "75AD32E1-1190-4608-83B8-C7640C6612B1", 
   "8a947f69-9d99-43b2-8def-226070dd93cf", 
+  "67abec28-db3e-403a-9bb1-13621c2890ff"
 }
